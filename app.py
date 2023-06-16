f5c40a893247f082d8ff4b0063bec29fb5d19f16
Josephheckert <69606492+Josephheckert@users.noreply.github.com>
2023-06-16 15:12:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,36 @@ import copy
 
 
 if __name__ == "__main__":
+
+    team_a = [
+    {"name":""},
+    {"roster":[]},
+    [], # List of player names only
+    [], # List of guardian(s) names only
+    [], # List of experienced players
+    []  # List of non-experienced players
+    ] 
+    team_b = [
+    {"name":""},
+    {"roster":[]},
+    [], # List of player names only
+    [], # List of guardian(s) names only
+    [], # List of experienced players
+    []  # List of non-experienced players
+    ] 
+    team_c = [
+    {"name":""},
+    {"roster":[]},
+    [], # List of player names only
+    [], # List of guardian(s) names only
+    [], # List of experienced players
+    []  # List of non-experienced players
+    ] 
+    league = [team_a, team_b, team_c]
+
+
+
+
     def clean_data():
         players_copy = copy.deepcopy(constants.PLAYERS)
         for player in players_copy:
@@ -33,67 +63,145 @@ if __name__ == "__main__":
         return non_experienced_players
     
 
-    def create_teams(team_a, team_b, team_c, players_copy, experienced_players, non_experienced_players):
+    def create_teams(league, team_a, team_b, team_c, players_copy, experienced_players, non_experienced_players):
         teams_copy = copy.deepcopy(constants.TEAMS)
-        players_per_team = len(players_copy) / len(teams_copy)
-        exp_players_per_team = len(experienced_players) / len(teams_copy)
-        nonexp_players_per_team = len(non_experienced_players) / len(teams_copy)
         team_a[0]["name"] = teams_copy[0]
         team_b[0]["name"] = teams_copy[1]
         team_c[0]["name"] = teams_copy[2]
-        # Add experienced players to team rosters
-        while len(team_a[1]["exp_players"]) < exp_players_per_team:
-            add_exp_player = experienced_players[0]
-            team_a[1]["exp_players"].append(add_exp_player)
-            experienced_players.pop(0)
-        while len(team_b[1]["exp_players"]) < exp_players_per_team:
-            add_exp_player = experienced_players[0]
-            team_b[1]["exp_players"].append(add_exp_player)
-            experienced_players.pop(0)
-        while len(team_c[1]["exp_players"]) < exp_players_per_team:
-            add_exp_player = experienced_players[0]
-            team_c[1]["exp_players"].append(add_exp_player)
-            experienced_players.pop(0)
-        # Add non-experienced players to team rosters
-        while len(team_a[2]["nonexp_players"]) < nonexp_players_per_team:
-            add_nonexp_player = non_experienced_players[0]
-            team_a[2]["nonexp_players"].append(add_nonexp_player)
-            non_experienced_players.pop(0)
-        while len(team_b[2]["nonexp_players"]) < nonexp_players_per_team:
-            add_nonexp_player = non_experienced_players[0]
-            team_b[2]["nonexp_players"].append(add_nonexp_player)
-            non_experienced_players.pop(0)
-        while len(team_c[2]["nonexp_players"]) < nonexp_players_per_team:
-            add_nonexp_player = non_experienced_players[0]
-            team_c[2]["nonexp_players"].append(add_nonexp_player)
-            non_experienced_players.pop(0)
+        exp_players_count = 0
+        nonexp_players_count = 0
+        players_per_team = len(players_copy) / len(teams_copy)
+        exp_players_per_team = len(experienced_players) / len(teams_copy)
+        nonexp_players_per_team = len(non_experienced_players) / len(teams_copy)
+        for team in league:
+            while len(team[1]["roster"]) < players_per_team:
+                if exp_players_count < exp_players_per_team:
+                    exp_player = experienced_players[0]
+                    team[1]["roster"].append(exp_player)
+                    team[2].append(exp_player["name"])
+                    team[3].append(exp_player["guardians"])
+                    team[4].append(exp_player)
+                    experienced_players.pop(0)
+                    exp_players_count += 1
+                else:
+                    break
+                if nonexp_players_count < nonexp_players_per_team:
+                    nonexp_player = non_experienced_players[0]
+                    team[1]["roster"].append(nonexp_player)
+                    team[2].append(nonexp_player["name"])
+                    team[3].append(nonexp_player["guardians"])
+                    team[5].append(nonexp_player)
+                    non_experienced_players.pop(0)
+                    nonexp_players_count += 1
+                else:
+                    break
+                exp_players_count = 0
+                nonexp_players_count = 0
         return team_a, team_b, team_c
     
 
-    def display_league(league):
-        for team in league:
-            print(f'''  
-                    Team name: {team[0]["name"]}
-                    Players On Team: {len(team[1]["exp_players"]) + len(team[2]["nonexp_players"])}
-                        Experienced Players: {len(team[1]["exp_players"])}
-                        Non-Experienced Players: {len(team[2]["nonexp_players"])}
-                  ''')
-            print("Roster:")
-            for player in team[1]["exp_players"]:
-                print(f'''{team[1]["exp_players"][0]["name"]}''')
+    def menu_select():
+        print(f"""---- MENU -----
+                  
+  Here are your choices:
+    A) Display Team Stats
+    B) Quit
+""")
+        main_menu_choice = input("Enter an option:    ").lower()
+        if main_menu_choice != "a" and main_menu_choice != "b":
+            print("Sorry, that's an invalid selection. Please choose one of the available options.\n")
+        else:
+            main_menu = main_menu_choice
+            return main_menu
 
-team_a = [{"name":""},{"exp_players":[]},{"nonexp_players":[]}]
-team_b = [{"name":""},{"exp_players":[]},{"nonexp_players":[]}]
-team_c = [{"name":""},{"exp_players":[]},{"nonexp_players":[]}]
+
+    def print_team(team_print):
+        continue_prog = "y"
+        guardians_list = ''
+        for guardian in team_print[3]:
+            guardians_list += ", ".join(guardian) + ", "
+        guardians_list = guardians_list[:-2]
+        player_count = len(team_print[4]) + len(team_print[5])
+        experienced_players_count = len(team_print[4])
+        non_experienced_players_count = len(team_print[5])
+        key = "height"
+        agg_height = 0
+        for player in team_print[1]["roster"]:
+            if key in player:
+                agg_height += int(player["height"])
+        avg_height = agg_height / player_count
+        print(f'''
+---- ROSTER ----
+              
+PLAYERS: 
+    {", ".join(team_print[2])}
+    
+GUARDIANS:''')
+
+        print(f'''    {guardians_list}
+
+STATS:
+    Player Count: {player_count}
+    Experienced Players: {experienced_players_count}
+    Non-Experienced Players: {non_experienced_players_count}
+    Average Height: {avg_height}
+    ''')
+
+
+    def continue_program():
+        continue_prog = "y"
+        while continue_prog == "y":
+            prompt = input(f'''
+Would you like to continue?  Y/N    ''').lower()
+            if prompt != "y" and prompt != "n":
+                print("Sorry, that's not a valid entry. Please enter Y or N")
+            elif prompt == "n":
+                print("Have a great day! <END>")
+                continue_prog = "n"
+                return continue_prog
+            else:
+                continue_prog = "y"
+                print("\n")
+                return continue_prog
+
+
+    def display_league(league):
+        main_menu = menu_select()
+        while main_menu == "a":
+            print(f'''---- TEAMS ----
+                  
+    A) {team_a[0]["name"]}
+    B) {team_b[0]["name"]}
+    C) {team_c[0]["name"]}
+''')
+            team_choice = input("Enter an option:    ").lower()
+            if team_choice == "a":
+                team_print = team_a
+                print_team(team_print)
+                continue_prog = continue_program()
+                if continue_prog == "n":
+                    return
+                main_menu = menu_select()
+            elif team_choice == "b":
+                team_print = team_b
+                print_team(team_print)
+                continue_prog = continue_program()
+                if continue_prog == "n":
+                    return
+                main_menu = menu_select()
+            elif team_choice == "c":
+                team_print = team_c
+                print_team(team_print)
+                continue_prog = continue_program()
+                if continue_prog == "n":
+                    return
+                main_menu = menu_select()
+
 
 
 
 players_copy = clean_data()
 experienced_players = experienced_list(players_copy)
 non_experienced_players = non_experienced_list(players_copy)
-create_teams(team_a, team_b, team_c, players_copy, experienced_players, non_experienced_players)
-
-league = [team_a, team_b, team_c]
-
+create_teams(league, team_a, team_b, team_c, players_copy, experienced_players, non_experienced_players)
 display_league(league)
-
